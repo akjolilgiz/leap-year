8ad158121a866ae19633c4bcabf15dc33c168265
Akjol Jaenbai and Brian Nelson <akjol@>
2018-08-15 08:29:09 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 var leapYear = function(inputYear) {
 
-  if (inputYear % 400 === 0) {return true;}
-
-  else if (inputYear % 100 === 0) {return false;}
-  else if(inputYear % 4 === 0) {
+  if ((inputYear % 4 === 0) && (inputYear % 100 !== 0) || (inputYear % 400 === 0)) {
     return true;
-  }
-  else {
-  return false;
+  } else {
+    return false;
   }
 }
 
