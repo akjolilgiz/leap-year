df8fff6b32c6b7e0770a6cd913b4365be7f5150d
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-15 08:06:56 -0700

    add leapYear return for all years divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
-var learYear = function(inputYear) {
-  return false;
+var leapYear = function(inputYear) {
 
+  if (inputYear % 4 === 0) {
+    return true;
+  } else {
+  return false;
+  }
 }
 
 
 
 
-$(document).ready(function(){
-  $("form#leap-year").submit(function(event){
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
 
-    var userInput = ("input#year").val();
-    var output = learYear(userInput);
+    var userInput = $("input#year").val();
+
+    var output = leapYear(userInput);
     alert(output);
   });
 });
